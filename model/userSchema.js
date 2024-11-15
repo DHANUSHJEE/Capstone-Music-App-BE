41fdf3ebe898c5e095ead39297f2b28a4c2050f2
Dhanush J S <dhanushjayakumar2@gmail.com>
2024-11-15 16:48:20 +0530

    updated

--- model/userSchema.js
+++ model/userSchema.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
+    password:{type:String,required:true},
     playlists: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Playlist' }]  // This should be an array of Playlist IDs
 });
 
